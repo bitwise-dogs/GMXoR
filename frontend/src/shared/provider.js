d025fec8e60b85e70de0a2df3ff867a68b178602
Alekseev_A <solmir505@gmail.com>
2024-10-25 14:43:48 +0800

    update provider.js

--- frontend/src/shared/provider.js
+++ frontend/src/shared/provider.js
@@ -1,10 +1,7 @@
-// src/shared/provider.js
 import { ethers } from 'ethers';
 
-// URL RPC провайдера
-//const PROVIDER_URL = 'https://eth.drpc.org';
-
-// Создаем и экспортируем провайдера для использования в других модулях
-const provider = new ethers.BrowserProvider(window.ethereum);
+const provider = typeof window !== "undefined" && window.ethereum
+  ? new ethers.BrowserProvider(window.ethereum)
+  : null;
 
 export default provider;
