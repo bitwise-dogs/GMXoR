4157d9ad49196313590f5a632b2c47eb0e7f1b67
Alekseev_A <solmir505@gmail.com>
2024-10-25 14:45:34 +0800

    update app.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,4 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from './app/App';
 
 
-
+beforeAll(() => {
+  global.window = Object.create(window);
+  Object.defineProperty(window, 'ethereum', {
+    value: {}, 
+  });
+});
