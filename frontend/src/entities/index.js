f86489d9c75c334ac6ae1571b576e658c9cfec7a
frayzkilla <frayzthamotion@gmail.com>
2024-11-13 16:29:52 +0800

    очистил entities

--- frontend/src/entities/index.js
+++
@@ -1,3 +0,0 @@
-export const datastore = '0xFD70de6b91282D8017aA4E741e9Ae325CAb992d8';  
-export const account = '0x591b6F096281DD7b645767C96aC34863A4Df9a89'; //На проде юзать _walletAddress. Это акк левого чела, у которого есть открытые позиции
-    
